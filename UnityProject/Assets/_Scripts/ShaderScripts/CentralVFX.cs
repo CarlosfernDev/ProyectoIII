c2a7a100f5fa3dd3b0882f000c876954936a1aef
Sergio <serferrer03@gmail.com>
2024-02-28 12:30:48 +0100

    CentralDisappear v0.1

---
+++ UnityProject/Assets/_Scripts/ShaderScripts/CentralVFX.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class CentralVFX : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+    [SerializeField] private VisualEffect _smoke1, _smoke2, _dissolve, _smokeDissapear, _spawnCloud;
+    [SerializeField] private GameObject _eolicCentral, _nuclearCentral;
+    [SerializeField] private float _transitionDuration;
+    public static int PosID = Shader.PropertyToID("_MaskPos");
+
+    public IEnumerator Coroutine_DissolveCentral()
+    {
+        if(_smoke1 != null) _smoke1.Stop();
+        if(_smoke2 != null) _smoke2.Stop();
+        if (_dissolve != null) Invoke(nameof(PlayDissolve), 0.3f);
+        float lerp = 0;
+        while (lerp < 1)
+        {
+            _material.SetVector("_MaskPos", Vector3.Lerp( new Vector3 (0, 12, 0), new Vector3 (0, 2, 0), lerp ));
+            lerp += Time.deltaTime * _transitionDuration;
+            yield return null;
+        }
+        if (lerp >= 1)
+        { 
+            _smokeDissapear.Play();
+            yield return new WaitForSeconds(0.3f);
+            _nuclearCentral.SetActive(false);
+            _eolicCentral.SetActive(true);
+        }
+    }
+
+    public void PlayDissolve()
+    { 
+        _dissolve.Play();
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(nameof(Coroutine_DissolveCentral));
+    }
+    private void Start()
+    {
+        _material.SetVector("_MaskPos", new Vector3(0, 12, 0));
+        _eolicCentral.SetActive(false);
+        _nuclearCentral.SetActive(true);
+        _smoke1.Play();
+        _smoke2.Play();
+        Invoke(nameof(CallCoroutine), 1);
+    }
+}
