898bae6ce845a7bb6603f469603d7a0f974af73e
Francisco Miguel Sanz Gatzkiewicz <franmi.sanz.gatz@gmail.com>
2024-03-17 21:32:05 +0100

    boat

--- UnityProject/Assets/_Scripts/Minigames/ODS14/BoatInputManager.cs
+++ UnityProject/Assets/_Scripts/Minigames/ODS14/BoatInputManager.cs
@@ -6,14 +6,13 @@ using UnityEngine.InputSystem;
 
 public class BoatInputManager : MonoBehaviour, Inputs.IBoatMapActions
 {
-    private Inputs _playerInputs;
-    private Vector2 _rawMovementInput;
-    private Vector2 _rawTurnInput;
-    
-    public int NormInputMove { get; private set; }
-    public int NormInputTurn { get; private set; }
     public Vector3 MovementInput { get; private set; }
     public Vector3 TurnInput { get; private set; }
+
+    private Inputs _playerInputs;
+    
+    private Vector2 _rawMovementInput;
+    private Vector2 _rawTurnInput;
 
     private void Awake()
     {
@@ -35,18 +34,11 @@ public class BoatInputManager : MonoBehaviour, Inputs.IBoatMapActions
     {
         _rawMovementInput = context.ReadValue<Vector2>();
         MovementInput = new Vector3(0,0,_rawMovementInput.y);
-        // if (Mathf.Abs(_rawMovementInput.x) > 0.5f)
-        // {
-        //     NormInputMove = (int)(_rawMovementInput * Vector3.forward).normalized.x;
-        // }
-        // else
-        // {
-        //     NormInputMove = 0;
-        // }
     }
 
     public void OnTurn(InputAction.CallbackContext context)
     {
         _rawTurnInput = context.ReadValue<Vector2>();
+        TurnInput = new Vector3(0,_rawTurnInput.x,0);
     }
 }
--- UnityProject/Assets/_Scripts/Minigames/ODS14/BoatMovementManager.cs
+++ UnityProject/Assets/_Scripts/Minigames/ODS14/BoatMovementManager.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BoatMovementManager : MonoBehaviour
 {
-    [SerializeField] private BoatInputManager _inputManager;
+    [SerializeField] private TMP_Text velocityDebug;
+    
+    [SerializeField] private float topSpeed;
+    [SerializeField] private float forceMult;
+    [SerializeField] private float accel;
+    [SerializeField] private float turnSpd;
+    
+    private BoatInputManager _inputManager;
+    private Rigidbody _rb;
 
     private void OnEnable()
     {
@@ -14,11 +24,31 @@ public class BoatMovementManager : MonoBehaviour
 
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        Debug.Log(_inputManager.MovementInput);
+        velocityDebug.text = _rb.velocity.magnitude.ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddRelativeTorque(_inputManager.TurnInput * turnSpd, ForceMode.Force);
+        SpeedUp();
+        // _rb.AddRelativeForce(_inputManager.MovementInput * accel, ForceMode.Acceleration);
+    }
+
+    private void SpeedUp()
+    {
+        if (_inputManager.MovementInput == Vector3.zero) return;
+        Vector3 targetVelocity = _inputManager.MovementInput * topSpeed;
+        Vector3 force = (targetVelocity - _rb.velocity) * forceMult;
+
+        if (force.magnitude > forceMult)
+        {
+            force = force.normalized * forceMult;
+        }
+        _rb.AddRelativeForce(force);
     }
 }
