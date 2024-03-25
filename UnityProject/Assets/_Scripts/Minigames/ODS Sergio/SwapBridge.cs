ad214db55d78839bd66a53d3a095b139d0b9ebca
Sergio <serferrer03@gmail.com>
2024-03-25 10:07:35 +0100

    桥梁、里约热内卢和可持续发展目标9占85%

--- UnityProject/Assets/_Scripts/Minigames/ODS Sergio/SwapBridge.cs
+++ UnityProject/Assets/_Scripts/Minigames/ODS Sergio/SwapBridge.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing.Text;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwapBridge : MonoBehaviour, Iinteractable
@@ -7,6 +9,19 @@ public class SwapBridge : MonoBehaviour, Iinteractable
     [SerializeField] public bool _isInteractable;
     [SerializeField] private string _TextoInteraccion;
     [SerializeField] private int costMaterial;
+    [SerializeField] private float _transitionDuration;
+
+    [SerializeField] private Material _GhostMat;
+    [SerializeField] private Material _BridgeWoodMat;
+    [SerializeField] private Material _BridgeWoodMat2;
+    [SerializeField] private Material _BridgeGoldMat;
+    
+    [SerializeField] private Renderer _rendererWood1;
+    [SerializeField] private Renderer _rendererWood2;
+    [SerializeField] private Renderer _rendererWoodMat2;
+    [SerializeField] private Renderer _rendererGold;
+
+    private List<Renderer> _bridgeRenderer = new List<Renderer>();
     public string TextoInteraccion
     {
         get { return _TextoInteraccion; }
@@ -17,10 +32,21 @@ public class SwapBridge : MonoBehaviour, Iinteractable
         get { return _isInteractable; }
         set { _isInteractable = value; }
     }
-
+    
     public void Start()
     {
+        _GhostMat.SetFloat("_RectangleHeight", 0);
         _TextoInteraccion = "Coste de construccion: " + costMaterial;
+
+        _bridgeRenderer.Add(_rendererWood1);
+        _bridgeRenderer.Add(_rendererWood2);
+        _bridgeRenderer.Add(_rendererWoodMat2);
+        _bridgeRenderer.Add(_rendererGold);
+
+        foreach (Renderer r in _bridgeRenderer)
+        {
+            r.material = _GhostMat;
+        }
     }
     public void Interact()
     {
@@ -34,7 +60,6 @@ public class SwapBridge : MonoBehaviour, Iinteractable
         {
             Debug.Log("Necesito mas monedas");
         }
-        
         
     }
 
@@ -51,8 +76,35 @@ public class SwapBridge : MonoBehaviour, Iinteractable
 
     public void Swap()
     {
-        GameObject.Find("Player").GetComponent<TestInputs>().hideTextFunction();
+        if (GameObject.Find("Player") != null)
+        {
+            GameObject.Find("Player").GetComponent<TestInputs>().hideTextFunction();
+        }
         
-        Destroy(this.gameObject);
+        StartCoroutine(SwapBridgeCoroutine());
+    }
+
+    private IEnumerator SwapBridgeCoroutine()
+    {
+        float lerp = 0;
+        while (lerp < 1)
+        {
+            _GhostMat.SetFloat("_RectangleHeight", Mathf.Lerp(0f, 2f, lerp));
+            lerp += Time.deltaTime * _transitionDuration;
+            yield return null;
+        }
+        if (lerp >= 1)
+        { 
+            Invoke(nameof(SetNormalBridgeMat), 0.4f);
+        }
+    }
+
+    public void SetNormalBridgeMat()
+    {
+        _rendererGold.material = _BridgeGoldMat;
+        _rendererWood1.material = _BridgeWoodMat;
+        _rendererWood2.material = _BridgeWoodMat;
+        _rendererWoodMat2.material = _BridgeWoodMat2;
+        GetComponent<BoxCollider>().isTrigger = true;
     }
 }
